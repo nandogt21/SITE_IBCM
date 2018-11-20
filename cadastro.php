c6365eb9e4342840b6b32766efd0f753583c9dfa
PANDOR <PANDOR@PANDOR-PC>
2018-11-20 13:03:31 -0200

    Primeiro comit para o git .. SITE_LOGIN_CADASTRO

--- cadastro.php
+++ cadastro.php
@@ -5,25 +5,33 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
-<link rel="stylesheet" type="text/css" href="css.css"
+<style>
+    body{
+        background-color:#333;
+    }
+    #cor_nome{
+        color:white;
+    }
+</style>
+
 </head>
 <body>
  
     
 <div class="container" >
 	<div class="row">
-            <center><h2>CRIE SEU CADASTRO ADMINISTRATIVO.</h2></center>
+            <center id="cor_nome"><h2>CRIE SEU CADASTRO ADMINISTRATIVO.</h2></center>
 	</div>
 </div>
     <form class="form-horizontal" method="POST" action="valida_cadastro.php">
 <fieldset >
 
-<!-- Form Name -->
-<legend><p>Area Administrativa</p></legend>
+
+    <a id="cor_nome"><center><p><h3>Area Administrativa</h3></p></center></a><br>
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Nome</label>  
+  <label class="col-md-4 control-label" for="textinput" id="cor_nome">Nome</label>  
   <div class="col-md-4">
   <input id="textinput" name="nome" type="text" placeholder="Insira seu nome completo" class="form-control input-md">
     
@@ -32,7 +40,7 @@
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">E-mail</label>  
+  <label class="col-md-4 control-label" for="textinput" id="cor_nome">E-mail</label>  
   <div class="col-md-4">
   <input id="textinput" name="email" type="text" placeholder="Digite seu melhor e-mail" class="form-control input-md">
     
@@ -41,7 +49,7 @@
 
 <!-- Text input-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Senha</label>
+  <label class="col-md-4 control-label" for="textinput" id="cor_nome">Senha</label>
   <div class="col-md-4">
   <input id="textinput" name="senha" type="password" placeholder="Digite sua senha" class="form-control input-md">
     
